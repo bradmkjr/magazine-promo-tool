dc14b95559870b3ac6c002cad3ad84f5dff80227
Bradford Knowlton <brad@ucanbonline.net>
2017-11-17 13:49:44 -0700

    working copy with follow

--- src/app.js
+++ src/app.js
@@ -42,6 +42,8 @@ var goldBoxDealsURL = 'http://www.feedrinse.com/services/rinse/?rinsedurl=ed26e7
 
 var promoCodesURL = 'https://affiliate-program.amazon.com/resource-center/amazon-promo-codes-for-associates/';
 
+var nodeURL = 'https://www.amazon.com/b/ref=sr_aj?node=';
+
 console.log('Starting Up!!!');
 
 /**
@@ -232,7 +234,7 @@ app.get('/subscribe', function(req, res) {
 	        	client.itemLookup({
 				  idType: 'ASIN',
 				  itemId: item.ASIN,
-				  responseGroup: 'Medium,EditorialReview'
+				  responseGroup: 'Medium,EditorialReview,BrowseNodes'
 					}, function(error, data, response) {
 					  if (error) {
 					    console.log(error);
@@ -279,7 +281,7 @@ app.get('/subscribe', function(req, res) {
 		
 		item.review = '';
 		
-		if( item.data[0].EditorialReviews[0] != undefined && item.data[0].EditorialReviews[0].EditorialReview[0].Content[0] != undefined ){
+		if( item.data[0].EditorialReviews != undefined && item.data[0].EditorialReviews[0].EditorialReview[0].Content[0] != undefined ){
 			item.review = item.data[0].EditorialReviews[0].EditorialReview[0].Content[0];
 		}
 		
@@ -296,6 +298,23 @@ app.get('/subscribe', function(req, res) {
 			item.image = item.data[0].LargeImage[0].URL[0];
 		}
 		
+		// console.log( item.data[0].BrowseNodes[0].BrowseNode[0].Name[0] );
+		
+		item.node = '';
+		if( item.data[0].BrowseNodes != undefined && item.data[0].BrowseNodes[0].BrowseNode[0].Name[0] != undefined ){
+			item.node = item.data[0].BrowseNodes[0].BrowseNode[0].Name[0];
+		}
+		
+		item.nodeId = '';
+		if( item.data[0].BrowseNodes != undefined && item.data[0].BrowseNodes[0].BrowseNode[0].BrowseNodeId[0] != undefined ){
+			item.nodeId = item.data[0].BrowseNodes[0].BrowseNode[0].BrowseNodeId[0];
+		}
+		
+		// console.log(item);
+		
+		
+		// 
+		
 		waterMark(item)
 		
 	} // end function processItem
@@ -310,9 +329,9 @@ app.get('/subscribe', function(req, res) {
 		    
 		    Jimp.read('./src/public/assets/img/twitter-circle.png').then(function (twitter) {
 		
-		    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK, function (err, fontLargeBlack) {
+		    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK, function (error, fontLargeBlack) {
 
-		    	Jimp.loadFont(Jimp.FONT_SANS_16_BLACK, function (err, fontSmallBlack){
+		    	Jimp.loadFont(Jimp.FONT_SANS_16_BLACK, function (error, fontSmallBlack){
 
 		    		image.composite( background, 0, ( image.bitmap.height - ( ( image.bitmap.height / 3 ) + 60 ) ) )
 		    		.composite( twitter, 28, ( image.bitmap.height - ( ( image.bitmap.height / 3 ) + 30 ) ) )
@@ -323,7 +342,7 @@ app.get('/subscribe', function(req, res) {
 
 			    			item.b64content = fs.readFileSync('./src/public/assets/img/image.processed.jpg', { encoding: 'base64' });
 
-			    			// console.log(b64content);
+			    			// console.log(item.b64content);
 
 			    			tweetWithMedia(item);	
 
@@ -338,14 +357,13 @@ app.get('/subscribe', function(req, res) {
 		    
 		    });	// end background image read
 
-		}).catch(function (err) {
+		}).catch(function (error) {
 		    // handle an exception 
-		    console.log(err);
+		    console.log(error);
 
 		});
 		
 	}
-	
 	
 	function tweetWithMedia(item){
 
@@ -354,57 +372,469 @@ app.get('/subscribe', function(req, res) {
 		// 	
 		 
 		// first we must post the media to Twitter 
-		T.post('media/upload', { media_data: item.b64content }, function (err, data, response) {
+		T.post('media/upload', { media_data: item.b64content }, function (error, data, response) {
 		  // now we can assign alt text to the media, for use by screen readers and 
 		  // other text-based presentations and interpreters 
-		  var mediaIdStr = data.media_id_string
-		  var meta_params = { media_id: mediaIdStr, alt_text: { text: item.title + ' ' + item.offer } }
-		 
-		  T.post('media/metadata/create', meta_params, function (err, data, response) {
-		    if (!err) {
-		      
-		      // now we can reference the media and post a tweet (media will attach to the tweet) 
-		      // this is the tweet message
-		      var tweet = { status: item.tweet, media_ids: [mediaIdStr] } 
-		 
-		      // console.log(tweet);
+		  
+		  if(error != undefined || response.statusCode != 200 ){
+		  	console.log("Something went wrong!");
+			console.log(error.message);
+			console.log(response.statusCode);
+			res.end();		  
+		  }else{
+			  
+			  var mediaIdStr = data.media_id_string
+			  var meta_params = { media_id: mediaIdStr, alt_text: { text: item.title + ' ' + item.offer } }
+			 
+			  T.post('media/metadata/create', meta_params, function (error, data, response) {
+			    if(error != undefined || response.statusCode != 200 ){
+			      	console.log(error);
+			    	console.log(response.statusCode);
+			    	
+			    	// error with media create
+			    	console.log('error creating meta data');
+			    		
+			    }else{
+					// now we can reference the media and post a tweet (media will attach to the tweet) 
+					// this is the tweet message
+					var tweet = { status: item.tweet, media_ids: [mediaIdStr] } 
+					
+					// console.log(tweet);
+					
+					T.post('statuses/update', tweet, function (error, data, response) {
 
-		      T.post('statuses/update', tweet, tweeted);
-
-		    }else{
-		    	console.log(err);
-		    	// error with media create
-		    	console.log('error creating meta data');
-		    }
-		  });
-
-		}); // save
+						    if(error != undefined || response.statusCode != 200 ){
+									console.log("Something went wrong!");
+									console.log(error.message);
+									console.log(response.statusCode);
+									res.end();
+							   
+							    }else{
+					
+						      // res.write('Voila It worked!');
+					
+						      T.get('statuses/oembed', { "id": data.id_str },  function (error, data, response) {
+						      					            
+						            if(error != undefined || response.statusCode != 200 ){
+										console.log("Something went wrong!");
+										console.log(error.message);
+										console.log(response.statusCode);
+										res.end();
+								   
+								    }else{
+										res.write(data.html);            
+										checkList(item);	            
+							        }
+					            
+					         }); // end statuses/oembed
+					
+						    } // end if
+					
+						}); // end statuses/update
+			    } // end if
+			  }); // end media/metadata/create	
+			  		  
+		  } // end if
+		}); // end media/upload
 
 	} // end tweetWithMedia
 	
-	function tweeted(err, data, response) {
+	
+	
+	
+	function checkList(item){
+	
+		if( item.node == undefined || item.nodeId == 0 ){
+			console.log('No Node');
+			return;
+		}
+	
+		console.log(item.node);
+		
+		console.log(item.nodeId);
+		
+		item.listTitle = item.node+' Magazines'; 
+		
+		item.listSlug = item.node.replace('&','and').replace(',','').replace(/\s+/g, '-')+'-Magazines'; 
+		
+		item.listDescription = item.node+' Magazine readers & fans. Browse more ' + item.listTitle + ' at '+ item.shortNodeURL;
+				
+		var replacements = [];
+		
+		replacements.push({ title_find: 'Horticulture-Magazines', 
+							title_replace: 'Culture-Mag',
+							desc_find: 'Gardening & Horticulture Magazine',
+							desc_replace: 'Gardening & Culture Magazine' });
+							
+		replacements.push({ title_find: 'Investing-Magazines',  // , 
+							title_replace: 'Invest-Mags',
+							desc_find: '',
+							desc_replace: '' });
 
-	    if(err){
+		replacements.push({ title_find: 'Motorcycles-Magazines', // 'Motorcycles-Magazines', 'Cycles-Mags'
+							title_replace: 'Cycles-Mags',
+							desc_find: '',
+							desc_replace: '' });	
+							
+		replacements.push({ title_find: 'History-and-Nature-Magazines', // 'History-and-Nature-Magazines', 'and-Nature-Mags'
+							title_replace: 'and-Nature-Mags',
+							desc_find: '',
+							desc_replace: '' });																						
 
-	      console.log("Something went wrong!");
+		replacements.push({ title_find: 'Other-Eastern-Religions-Magazines', // 'Other-Eastern-Religions-Magazines', 'Eastern-Religions-Mags'
+							title_replace: 'Eastern-Religions-Mags',
+							desc_find: 'Other Eastern Religions Magazine',
+							desc_replace: 'Eastern Religions Magazine' });
+							
 
-	      console.log(err.message);
-	   
-	    }else{
+		replacements.push({ title_find: 'City-and-Regional-Magazines', // 'City-and-Regional-Magazines', 'Regional-Magazines'
+							title_replace: 'Regional-Magazines',
+							desc_find: 'Travel, City & Regional Magazines',
+							desc_replace: 'Travel Regional Magazines' }); 
+							
+		replacements.push({ title_find: 'and-Hobbies-Magazines', // 'Hobbies-Magazines', 'Hobbies-Mags'
+							title_replace: 'Magazines',
+							desc_find: '',
+							desc_replace: '' });
+							
+		replacements.push({ title_find: 'Teen-Magazines', // 'Teen-Magazines', 'Teen-Mags'
+							title_replace: 'Teen-Mags',
+							desc_find: '',
+							desc_replace: '' });
+							
+		replacements.push({ title_find: 'Fitness-and-Wellness-Magazines', // 'Fitness-and-Wellness-Magazines', 'and-Wellness-Mags'
+							title_replace: 'and-Wellness-Mags',
+							desc_find: '',
+							desc_replace: '' });
+							
+		replacements.push({ title_find: 'Style-Magazines', // 'Style-Magazines', 'Style-Mags'
+							title_replace: 'Style-Mags',
+							desc_find: '',
+							desc_replace: '' });
 
-	      // res.write('Voila It worked!');
+		replacements.push({ title_find: 'Firearms-Magazines', // 'Firearms-Magazines', 'Firearms-Mags'
+							title_replace: 'Firearms-Mags',
+							desc_find: '',
+							desc_replace: '' });
 
-	      T.get('statuses/oembed', { "id": data.id_str },  function (err, data, response) {
-            
-            res.write(data.html);
+		replacements.push({ title_find: 's-and-Techniques-Magazines', // 's-and-Techniques-Magazines', '-Magazines'
+							title_replace: '-Magazines',
+							desc_find: '',
+							desc_replace: '' });
 
-            res.end();
-         });
+		replacements.push({ title_find: 'and-Decoration-Magazines', // 'and-Decoration-Magazines', 'Magazines'
+							title_replace: 'Magazines',
+							desc_find: '',
+							desc_replace: '' });
 
-	    }
+		replacements.push({ title_find: 'Nature-Magazines', // 'Nature-Magazines', 'Nature-Mags'
+							title_replace: 'Nature-Mags',
+							desc_find: '',
+							desc_replace: '' });
 
-	} // end tweeted
+		replacements.push({ title_find: 'Pets-and-Animals-Magazines', // 'Pets-and-Animals-Magazines', 'Animals-Magazines' 
+							title_replace: 'Animals-Magazines',
+							desc_find: '',
+							desc_replace: '' });
 
+		replacements.push({ title_find: 'Improvements-Magazines', // 'Improvements-Magazines', 'Magazines'
+							title_replace: 'Magazines',
+							desc_find: 'How-to & Home Improvements Magazine',
+							desc_replace: 'How-to & Home Magazine' });
+																												
+		replacements.push({ title_find: 'Music-and-Photography-Magazines', // 'Music-and-Photography-Magazines', 'and-Music-Magazines'
+							title_replace: 'and-Music-Magazines',
+							desc_find: 'Arts, Music & Photography Magazine',
+							desc_replace: 'Arts & Music Magazine' });
+
+		replacements.push({ title_find: 'and-Commentary-Magazines', // 'and-Commentary-Magazines', 'Magazines'
+							title_replace: 'Magazines',
+							desc_find: '',
+							desc_replace: '' });
+
+		replacements.push({ title_find: 'Cooking-Food-and-Wine-Magazines', 
+							title_replace: 'Cooking-and-Wine-Magazine',
+							desc_find: '',
+							desc_replace: '' });
+
+		replacements.push({ title_find: 'Art-and-Art-History-Magazines', 
+							title_replace: 'Art-and-History-Magazines',
+							desc_find: '',
+							desc_replace: '' });
+		
+		replacements.push({ title_find: 'Antiques-and-Collectibles-Magazines', 
+							title_replace: 'Antiques-Magazines',
+							desc_find: '',
+							desc_replace: '' });
+
+		replacements.push({ title_find: 'Sustainable-Living-Magazines', 
+							title_replace: 'Sustainable-Magazines',
+							desc_find: 'Sustainable Living Magazine',
+							desc_replace: 'Sustainable Magazine' });
+
+		replacements.push({ title_find: 'Wine-and-Cocktails-Magazines', 
+							title_replace: 'Wine-and-Drink-Magazines',
+							desc_find: 'Wine & Cocktails Magazine',
+							desc_replace: 'Wine & Drinks Magazine' });
+
+		replacements.push({ title_find: 'Entrepreneurship-Magazines', 
+							title_replace: 'Entrepreneurship-Magazine',
+							desc_find: 'Entrepreneurship-Magazines',
+							desc_replace: 'Entrepreneurship-Magazine' });
+
+		replacements.push({ title_find: '', 
+							title_replace: '',
+							desc_find: '',
+							desc_replace: '' });
+
+		
+		// console.log(replacements);
+		
+		console.log( 'Before Replacement: ' + item.listSlug );
+		console.log( 'Before Replacement: ' + item.listDescription );
+				
+		for(i = 0; i < replacements.length; i++){
+			
+				item.listSlug = item.listSlug.replace( replacements[i].title_find, replacements[i].title_replace );
+				
+				item.listDescription = item.listDescription.replace( replacements[i].desc_find, replacements[i].desc_replace )
+														   .replace( replacements[i].desc_find, replacements[i].desc_replace );
+			
+		}
+		
+		console.log( 'After Replacement: ' + item.listSlug );		
+		console.log( 'After Replacement: ' + item.listDescription );
+		
+		// console.log( item.listTitle );
+		// console.log( item.listSlug );
+		
+	  	item.nodeURL = nodeURL + item.nodeId + '&ajr=0&tag=' + process.env.amazon_tracking_id;
+	  	
+	  	console.log(item.nodeURL);	  	
+	  	
+	  	//
+		//  Creates a new list for the authenticated user. Note that you can create up to 1000 lists per account.
+		//
+		T.get('lists/list', {  reverse: true }, function(error, data, response) {
+		  // console.log(data);
+		  // console.log(data.statuses.length);
+		  // console.log(response);
+	
+		  if( data != undefined && !error && response.statusCode == 200 ){
+		  	// res.write(JSON.stringify(data));	
+		  	
+	 		  // console.log(data);
+	 		  item.list = {};
+	 		  
+	 		  var found = undefined;
+	 		  
+	 		  // console.log(data.length);
+	 		  
+	 		  for(i = 0; i < data.length; i++){
+		 		
+		 		// console.log(data[i]);
+		 		
+		 		if( data[i].name == item.listSlug ){
+			 		// list match
+			 		// console.log(data[i]);
+			 		item.list.id = data[i].id;
+			 		item.list.id_str = data[i].id_str;
+			 		item.list.list_id = data[i].id_str;
+			 		
+			 		found = true;
+		 		}	  
+		 		  
+	 		  }
+	 		  
+	 		  // console.log(found);
+	 		  
+	 		  if( found == undefined){
+		 		  
+		 		  console.log('List Not Found');
+		 		  createShortNodeURL(item);
+		 		  
+	 		  }else{
+		 		  console.log('List Found');
+		 		  console.log(item.list);
+		 		  
+		 		  searchTwitter(item);
+	 		  }
+	 		  
+		  } // end if
+		  else if( error )
+		  {
+			console.log(error);	
+			console.log(data);
+			console.log(response.statusCode);
+		  }
+		  
+		}); // end lists/create
+	  	
+	} // end function checkList
+	
+	function createShortNodeURL(item){
+		
+		cache.getCache(item.nodeURL, function(error,data){
+	       		if( error || data == undefined ){
+	        	// cache MISS
+	        	console.log('bitly: cache MISS');
+	        	// console.log(data);
+	        	
+	        	var bitly = new Bitly( process.env.bitly_access_token );
+	       
+		       bitly.shorten(item.nodeURL)
+				.then(function(response) {
+					// Do something with data 
+					item.shortNodeURL = response.data.url;
+		
+					console.log('Short Node URL: '+item.shortNodeURL);
+			
+					cache.setCache(item.nodeURL, response.data.url, function(){
+							console.log('bitly: cache SET');
+										
+							createList(item)
+						}); 		
+		
+				}, function(error) {
+					
+					throw error;
+				});
+	        
+	        }else{
+	        	// cache HIT
+	        	console.log('bitly: cache HIT');
+	        	
+	        	item.shortNodeURL = data;
+	        	console.log('Short Node URL: '+item.shortNodeURL);
+
+	        	createList(item)
+	        	
+	        }
+	        
+	      }); // end cache.getCache  
+		
+	} // end function createShortNodeURL
+
+
+	function createList(item){
+		
+		//
+		//  Creates a new list for the authenticated user. Note that you can create up to 1000 lists per account.
+		//
+		T.post('lists/create', { name: item.listSlug, description: item.listDescription  }, function(error, data, response) {
+		  // console.log(data);
+		  // console.log(data.statuses.length);
+		  // console.log(response);
+	
+		  if( data != undefined && !error && response.statusCode == 200 ){
+		  	// res.write(JSON.stringify(data));	
+		  	
+	 		  item.list.id = data.id;
+			  item.list.id_str = data.id_str;
+			  item.list.list_id = data.id_str;
+			  
+			  // item.list.owner_id = data.user.id_str;
+			 	
+			  console.log(item.list);
+			  
+			  searchTwitter(item)		
+		  	
+		  } // end if
+		  else if( error )
+		  {
+			console.log(error);	
+			console.log(data);
+			console.log(response.statusCode);
+		  }
+		  
+	
+		}); // end lists/create
+		
+	}
+	
+	function searchTwitter(item){
+	
+		console.log('Twitter Search: '+ item.title);
+	
+		//
+		//  search twitter for all tweets containing the word '#coupon'
+		//
+		T.get('search/tweets', { q: item.title, count: 1, result_type: 'recent' }, function(error, data, response) {
+		  // console.log(data);
+		  // console.log(error);
+		  // console.log(response);
+	
+		  if( data != undefined && !error && response.statusCode == 200 && data.statuses.length != 0 ){
+		  	// res.write(JSON.stringify(data));	
+		  	
+		  	var status = data.statuses[0];
+	
+		  	console.log(status.text);
+		  	res.write( 'Tweet: ' + status.text );	
+	
+		  	// res.write( status.user.id_str );	
+		  	res.write( 'User: ' + status.user.name );	
+		  	// console.log( status.user.screen_name );	
+		  	res.write( 'Screen Name: ' + status.user.screen_name );	
+		  	
+		  	T.post('friendships/create', { user_id: status.user.id_str }, function(error, data, response) {
+	
+		  		// console.log(data);
+		  		// console.log(response);
+	
+		  		if( data != undefined && !error && response.statusCode == 200 ){
+	
+		  			// console.log(response);
+		  			console.log('Following Status: ' + data.following);
+		  			res.write( 'Following Status: ' + data.following );	
+
+		  			console.log('Item: ' + item);
+	
+		  			console.log('List ID: '+ item.list.list_id);
+	
+					T.post('lists/members/create', { list_id: item.list.list_id, user_id: status.user.id_str, screen_name: status.user.screen_name }, function(error, data, response) {
+	
+				  		// console.log(data);
+				  		// console.log(response);
+	
+				  		if( data != undefined && !error && response.statusCode == 200 ){
+	
+				  			// console.log(response);
+				  			console.log( data.member_count );
+				  			res.write( 'Member Count: '+data.member_count );	
+				  			// console.log(data);
+				  		}
+				  		else if( error )
+				  		{
+							console.log(error);	  			
+				  		}
+	
+				  		res.end();	
+				  	});
+	
+		  		}
+		  		else if( error )
+		  		{
+					console.log(error);	  			
+		  		}
+	
+		  		// res.end();	
+	
+		  	});
+		  	
+		  } // end if
+		  else if( error )
+		  {
+			console.log(error);	
+			console.log(data);
+			console.log(response.statusCode);
+		  }
+		  
+	
+		}); // end search/tweets
+		
+		
+	} // end function searchTwitter
 
 }); // end get /subscribe
 
